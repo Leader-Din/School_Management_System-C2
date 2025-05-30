00bdd187c45136db377d7352861309dfd9f4bb07
Lynak <lynak.khat@student.passerellesnumeriques.org>
2025-05-30 13:30:10 +0700

    update code in googleclassroom

--- src/GoogleClassroom.ts
+++ src/GoogleClassroom.ts
@@ -1,3 +1,15 @@
+import type { Assignment } from "./Assignment";
+import type { Grade } from "./Grade";
+import type { StudyMaterial } from "./StudyMaterial";
+
 export class GoogleClassroom {
-    
+    private studyMaterials: StudyMaterial[] = [];
+    private assignments: Assignment[] = [];
+    private grades: Grade[] = [];
+    private teachers : Teacher [] = [];
+    private students : Student[] = [];
+
+    constructor(private name:string){
+        
+    }
 }
