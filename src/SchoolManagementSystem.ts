fe73eba303f59a4263dd10ea4ea8cb5aa4288ae6
Leader-Din <leader.din@student.passerellesnumeriques.org>
2025-05-30 12:53:48 +0700

    setup user and school

--- src/SchoolManagementSystem.ts
+++ src/SchoolManagementSystem.ts
@@ -1,3 +1,7 @@
 export class SchoolManagementSystem {
-    
+    constructor (private name : string, private address : string, private contact : string) {}
+
+    public viewSchoolDetails() : string {
+        return `School Name: ${this.name}, Address: ${this.address}, Contact: ${this.contact}`;
+    }
 }
---
+++ src/User.ts
@@ -0,0 +1,26 @@
+export abstract class User {
+    constructor (private userId : number, private firstName : string, private lastName : string, private email : string, private password : string, private phone : string) {}
+
+    public register (firstName: string, lastName: string, email: string, password: string, phone: string): void {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public login (email: string, password: string): boolean {
+        if (this.email === email && this.password === password) {
+            console.log("Login successful");
+            return true;
+        } else {
+            console.log("Login failed");
+            return false;
+        }
+    }
+
+    public logout (): void {
+        
+    }
+    
+}
