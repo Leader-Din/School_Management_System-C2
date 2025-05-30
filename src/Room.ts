bf2362ef3190e484a8e912e117a041dd5d1566c0
nilrathana <nilrathana.tep@gmail.com>
2025-05-30 16:41:31 +0700

    update Grade

--- src/Room.ts
+++ src/Room.ts
@@ -1,10 +1,20 @@
 export class Room {
-    className: string;
+    classNames: string[] = [];
     classLocation: string;
 
-  constructor(className: string, classLocation: string) {
-    this.className = className;
+  constructor(classLocation: string) {
     this.classLocation = classLocation;
+  }
+  addRoom(className: string, classLocation: string): void {
+    if (this.classLocation !== classLocation) {
+      console.log(`Warning: Room already has location set to ${this.classLocation}.`);
+    }
+    if (!this.classNames.includes(className)) {
+      this.classNames.push(className);
+      console.log(`Class "${className}" added to room at ${this.classLocation}`);
+    } else {
+      console.log(`Class "${className}" already exists in this room.`);
+    }
   }
 }
 
