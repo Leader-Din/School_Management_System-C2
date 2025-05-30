b9a12a354a57e5024c4c1fa9509c7d3a6a9a0abc
Leader-Din <leader.din@student.passerellesnumeriques.org>
2025-05-30 13:54:03 +0700

    subject student teacher

--- src/Teacher.ts
+++ src/Teacher.ts
@@ -1,5 +1,5 @@
 import { User } from "./User";
 
-class Teacher extends User {
+export class Teacher extends User {
     
 }
--- src/TimeTable.ts
+++ src/TimeTable.ts
@@ -1,3 +1,12 @@
+import type { Exam } from "./Exam";
+
 export class TimeTable {
-    constructor (private time : Date, private classroom : string, private subject : string, private examShedules : string[]) {}
+    constructor (
+        private time : Date, private room : string, 
+        private subject : string, private examShedules : Exam[]
+    ) {}
+
+    public  addExamSchedule(examSchedules: Exam): void {
+        this.examShedules.push(examSchedules);
+    }
 }
