ee62ce0f57537d4516d0ea47ffd06785ff39643f
Leader-Din <leader.din@student.passerellesnumeriques.org>
2025-05-30 13:12:45 +0700

    TimeTable and Exam

---
+++ src/Exam.ts
@@ -0,0 +1,4 @@
+export class Exam {
+
+    constructor (private subjects : string [], private title : string, private date : Date, private room : string[]) {}
+}
---
+++ src/TimeTable.ts
@@ -0,0 +1,3 @@
+export class TimeTable {
+    constructor (private time : Date, private classroom : string, private subject : string, private examShedules : string[]) {}
+}
