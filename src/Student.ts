382f4f3e2948bc6cdb96c6245d0e5b21aa2acf9b
Leader-Din <leader.din@student.passerellesnumeriques.org>
2025-05-30 15:20:05 +0700

    change some feature

--- src/Student.ts
+++ src/Student.ts
@@ -1,36 +1,40 @@
+import { User } from "./User";
+import type { Assignment } from "./Assignment";
 import type { GoogleClassroom } from "./GoogleClassroom";
 import type { TimeTable } from "./TimeTable";
-import { User } from "./User";
 
-export class Student extends User{
-    constructor (
-        userId : number, firstName : string, 
-        lastName : string, email : string, 
-        password : string, phone : string, 
-        private assignments : string[], 
-        private timetable : TimeTable,
-        private googleClassroom : GoogleClassroom
+export class Student extends User {
+    constructor(
+        userId: number,
+        firstName: string,
+        lastName: string,
+        email: string,
+        password: string,
+        phone: string,
+        private assignments: Assignment[],
+        private timetable: TimeTable,
+        private googleClassroom: GoogleClassroom
     ) {
         super(userId, firstName, lastName, email, password, phone);
     }
 
-    public viewTimetable() : TimeTable {
+    public viewTimetable(): TimeTable {
         return this.timetable;
     }
 
-    public viewExamSchedules() : TimeTable {
+    public viewExamSchedules(): TimeTable {
         return this.timetable;
     }
 
-    public viewStudyMaterial() : GoogleClassroom {
+    public viewStudyMaterial(): GoogleClassroom {
         return this.googleClassroom;
     }
 
-    public viewAssignment() : GoogleClassroom {
+    public viewAssignment(): GoogleClassroom {
         return this.googleClassroom;
     }
 
-    public viewAssignmentGrade() : GoogleClassroom {
+    public viewAssignmentGrade(): GoogleClassroom {
         return this.googleClassroom;
     }
 }
--- src/Teacher.ts
+++ src/Teacher.ts
@@ -1,12 +1,16 @@
-import type { Department } from "./Department";
 import { User } from "./User";
+import type { Department } from "./Department";
+
 export class Teacher extends User {
-   constructor (
-
-    userId : number, firstName : string, lastName : string, email : string, password : string, phone : string, private department : Department 
-   ) {
-    super (userId, firstName, lastName, email, password, password)
-   }
-
-
+    constructor(
+        userId: number,
+        firstName: string,
+        lastName: string,
+        email: string,
+        password: string,
+        phone: string,
+        private department: Department
+    ) {
+        super(userId, firstName, lastName, email, password, phone);
+    }
 }
